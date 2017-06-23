299271bb3f31272ba04c86869ce59efda260b8a2
jacksson.mosquera@offcorss.com <1@offcorss.com>
2017-06-23 08:56:26 -0500

    Primera Carga

---
+++ Hermeco.Compensacion.Cumpleanos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Hermeco.Compensacion.Cumpleanos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpleadosSAP.EmpleadosSAPServiceClient client = new EmpleadosSAP.EmpleadosSAPServiceClient();    
+        
+            Hermeco.Compensacion.Cumpleanos.EmpleadosSAP.GetEmployeeIntranetResponse empleadosCumple = client.GetBirthdaysOfTheWeek();
+
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("es-CO");
+
+            var listEmpleadosCumpleHoy = empleadosCumple.EmployeeIntranet.Where(x => x.BirthDay.ToLower().Equals(DateTime.Now.ToString("dd MMMM").TrimStart('0').ToLower())).ToList();
+            
+            //Enviar correo
+            foreach(var empleado in listEmpleadosCumpleHoy)
+            {
+                
+            }
+        }
+    }
+}
