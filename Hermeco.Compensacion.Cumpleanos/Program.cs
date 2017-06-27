45ce7b89c0a2218bc75c9d4f972579272c09e183
jacksson.mosquera@offcorss.com <1@offcorss.com>
2017-06-27 08:37:32 -0500

    Imagen de Email

--- Hermeco.Compensacion.Cumpleanos/Program.cs
+++ Hermeco.Compensacion.Cumpleanos/Program.cs
@@ -23,11 +23,13 @@ namespace Hermeco.Compensacion.Cumpleanos
 
             string fromEmail = ConfigurationManager.AppSettings["FromEmail"].ToString();
             string smtpServer = ConfigurationManager.AppSettings["SMTPServer"].ToString();
-
+            string subject = string.Empty;
             //Enviar correo
             foreach(var empleado in listEmpleadosCumpleHoy)
             {
-                Utility.sendEmail("", "", "", fromEmail, empleado.EMail, "", true, "", null);                
+                subject = string.Format("¡{0} Feliz Cumpleaños te desea tu familia OFFCORSS!", empleado.FirstName.Split(' ')[0]);
+                //Utility.sendEmail(smtpServer, subject, "", "", fromEmail, empleado.EMail, "", true, "", null);
+                Utility.sendEmail(smtpServer, subject, "", "", fromEmail, "analu0409@gmail.com", "", true, "", null);                
             }
         }
     }
