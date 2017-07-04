4ec8758299f49b3bd79607822a2eb7fede9defba
jacksson.mosquera@offcorss.com <1@offcorss.com>
2017-07-04 15:38:43 -0500

    Se configura la imagen embebida para que se descarge de una vez

--- Hermeco.Compensacion.Cumpleanos/Program.cs
+++ Hermeco.Compensacion.Cumpleanos/Program.cs
@@ -26,10 +26,10 @@ namespace Hermeco.Compensacion.Cumpleanos
             string subject = string.Empty;
             //Enviar correo
             foreach(var empleado in listEmpleadosCumpleHoy)
-            {
-                subject = string.Format("¡{0} Feliz Cumpleaños te desea tu familia OFFCORSS!", empleado.FirstName.Split(' ')[0]);
-                //Utility.sendEmail(smtpServer, subject, "", "", fromEmail, empleado.EMail, "", true, "", null);
-                Utility.sendEmail(smtpServer, subject, "", "", fromEmail, "analu0409@gmail.com", "", true, "", null);                
+            {                
+                subject = string.Format("¡{0}, Feliz Cumpleaños te desea la familia OFFCORSS!", empleado.FirstName.Trim().Split(' ')[0]);                
+                Utility.sendEmail(smtpServer, subject, AppDomain.CurrentDomain.BaseDirectory +"\\Email\\Cumpleanos.html", "", fromEmail, empleado.EMail, "", true, "", null);
+                                
             }
         }
     }
--- Hermeco.Compensacion.Cumpleanos/Utility.cs
+++ Hermeco.Compensacion.Cumpleanos/Utility.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Mail;
+using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,15 +14,22 @@ namespace Hermeco.Compensacion.Cumpleanos
         public static void sendEmail(string smtpServer, string subject, string htmlPath, string body, string from, string to, string pathAttachment, bool isBodyHtml, string CC, Dictionary<string, string> parameters = null)
         {
 
-            MailMessage mail = new MailMessage(from, to);
-
-            SmtpClient client = new SmtpClient() { Port = 25, DeliveryMethod = SmtpDeliveryMethod.Network, UseDefaultCredentials = false, Host = smtpServer };            
-
+            MailMessage mail = new MailMessage(from, to) { From = new MailAddress(from, "Yo Soy Cultura OFFCORSS"), IsBodyHtml = isBodyHtml, Subject = subject };
+            
+            SmtpClient client = new SmtpClient() { Port = 25, DeliveryMethod = SmtpDeliveryMethod.Network, UseDefaultCredentials = false, Host = smtpServer };
+            
             if (isBodyHtml)
             {
+                mail.IsBodyHtml = true;
+
                 using (StreamReader reader = File.OpenText(htmlPath))
                 {
-                    mail.Body = reader.ReadToEnd();                    
+                    var inlineLogo = new LinkedResource( AppDomain.CurrentDomain.BaseDirectory+"Email//img//E-card-Cumple.jpg");
+                    inlineLogo.ContentId = Guid.NewGuid().ToString();
+                    string body2 = string.Format(@reader.ReadToEnd(), inlineLogo.ContentId);
+                    var view = AlternateView.CreateAlternateViewFromString(body2, null, "text/html");
+                    view.LinkedResources.Add(inlineLogo);
+                    mail.AlternateViews.Add(view);                    
                 }
             }
             else
